eb48ec36b31a7f88512f5a59750c381d8037379c
road <rod8711@gmail.com>
2023-07-03 22:39:45 +0700

    move new/import button on top bar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,17 @@
 "use client";
-import { Tabs } from "antd";
-import { ReactNode, useMemo, useState } from "react";
+import { Button, Modal } from "antd";
+import { ReactNode, useRef, useState } from "react";
 import {
     ChatterinoDragSplitPreview,
     ChatterinoSplitAdvanced,
     ChatterinoSplitResize,
     ChatterinoSplitVertical,
 } from "@/app/fake-uis/chatterino";
-import { ConfigContextProvider } from "@/app/color-context-provider";
+import {
+    ConfigContextProvider,
+    ThemeData,
+    useConfigContext,
+} from "@/app/color-context-provider";
 import { MessageSettings } from "@/app/settings/messageSettings";
 import { ScrollBarSettings } from "@/app/settings/scrollBarSettings";
 import { TabsSettings } from "@/app/settings/tabsSettings";
@@ -21,6 +25,13 @@ import { SplitSettings } from "@/app/settings/splitSettings";
 import { ChatterinoTabPreview } from "@/app/fake-uis/chatterinoTabPreview.component";
 import { WindowSettings } from "@/app/settings/windowSettings";
 import { OverviewSettings } from "@/app/settings/overviewSettings";
+import {
+    CHATTERINO_BLACK_THEME,
+    CHATTERINO_DARK_THEME,
+    CHATTERINO_LIGHT_THEME,
+    CHATTERINO_WHITE_THEME,
+} from "@/resources";
+import { css2qt, qt2css } from "@/utils";
 
 interface Tab {
     label: string;
@@ -28,120 +39,53 @@ interface Tab {
     children: ReactNode;
 }
 
+function downloadFile(text: string, fileName: string) {
+    const a = document.createElement("a");
+    a.href = "data:text/plan;charset=utf-8," + encodeURIComponent(text);
+    a.download = fileName;
+    a.style.display = "none";
+
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+}
+
+function ExportButton() {
+    const { data } = useConfigContext();
+    return (
+        <Button
+            className="border-gray-200 text-gray-800 bg"
+            onClick={() => {
+                downloadFile(JSON.stringify(css2qt(data)), "theme.json");
+            }}
+        >
+            {" "}
+            Export{" "}
+        </Button>
+    );
+}
+
 export default function Home() {
     const [activeTab, setActiveTab] = useState("overview");
     const [activePreviewTab, setActivePreviewTab] = useState("chat");
-    const tabs = useMemo(
-        () => [
-            {
-                label: "Overview",
-                key: "overview",
-                children: <OverviewSettings />,
-            },
-            {
-                label: "Messages",
-                key: "messages",
-                children: <MessageSettings />,
-            },
-            {
-                label: "Scrollbars",
-                key: "scrollbars",
-                children: <ScrollBarSettings />,
-            },
-            {
-                label: "Splits",
-                key: "splits",
-                children: (
-                    <div className="max-h-full overflow-y-scroll">
-                        <SplitSettings />
-                    </div>
-                ),
-            },
-            {
-                label: "Tabs",
-                key: "tabs",
-                children: <TabsSettings />,
-            },
-            {
-                label: "Window",
-                key: "window",
-                children: <WindowSettings />,
-            },
-        ],
-        []
-    );
-    const previewTabs: Tab[] = [
-        {
-            label: "Chat",
-            key: "chat",
-            children: (
-                <div className="p-4 bg-gray-800 text-white h-full overflow-hidden flex flex-col">
-                    <ChatterinoSplitVertical
-                        chatMessages={fakeChatListLarge}
-                        extraClasses={clsx("h-[1000px]")}
-                    />
-                </div>
-            ),
-        },
-        {
-            label: "Split Drop Preivew",
-            key: "spilt_drop_preview",
-            children: (
-                <div className="p-4 bg-gray-800 text-white h-full overflow-hidden">
-                    <div>
-                        Hover mouse around split & drop target to simulate
-                        dragging split
-                    </div>
-                    <ChatterinoDragSplitPreview
-                        chatMessages={fakeChatListSmall}
-                    />
-                </div>
-            ),
-        },
-        {
-            label: "Split Drop Target",
-            key: "spilt_drop_target",
-            children: (
-                <div className="p-4 bg-gray-800 text-white h-full overflow-hidden flex flex-col">
-                    <div>
-                        Hover mouse around split & drop target to simulate
-                        dragging split
-                    </div>
-                    <ChatterinoSplitAdvanced
-                        chatMessages={fakeChatListVerySmall}
-                    />
-                </div>
-            ),
-        },
-        {
-            label: "Split Resize",
-            key: "spilt_resize",
-            children: (
-                <div className="p-4 bg-gray-800 text-white h-full overflow-hidden flex flex-col">
-                    <ChatterinoSplitResize
-                        chatMessages={fakeChatListVerySmall}
-                    />
-                </div>
-            ),
-        },
-        {
-            label: "Tab States",
-            key: "tab_states",
-            children: (
-                <div className="p-4 bg-gray-800 h-full overflow-auto flex flex-col">
-                    <ChatterinoTabPreview />
-                </div>
-            ),
-        },
-    ];
+    const [isOpen, setOpen] = useState(false);
+
     return (
         <ConfigContextProvider>
             <div className="h-full flex flex-col">
-                <div className="px-4 border-b border-b-gray-200 flex-shrink-0">
+                <div className="px-4 border-b border-b-gray-200 flex-shrink-0 flex items-center space-x-2">
                     <div className="text-2xl font-bold py-2">
-                        {" "}
                         Chatterino Theme Creator
                     </div>
+                    <div className="flex-grow"></div>
+                    <Button
+                        type="primary"
+                        className="border-gray-200 text-gray-800 font-bold"
+                        onClick={() => setOpen(true)}
+                    >
+                        New
+                    </Button>
+                    <ExportButton />
                 </div>
                 {/*<div className="h-full bg-emerald-300"></div>*/}
                 {/*overflow-auto somehow force the height to be correct??*/}
@@ -189,6 +133,221 @@ export default function Home() {
                     </div>
                 </div>
             </div>
+            <ThemeModal isOpen={isOpen} setOpen={setOpen} />
         </ConfigContextProvider>
+    );
+}
+
+const tabs: Tab[] = [
+    {
+        label: "Overview",
+        key: "overview",
+        children: <OverviewSettings />,
+    },
+    {
+        label: "Messages",
+        key: "messages",
+        children: <MessageSettings />,
+    },
+    {
+        label: "Scrollbars",
+        key: "scrollbars",
+        children: <ScrollBarSettings />,
+    },
+    {
+        label: "Splits",
+        key: "splits",
+        children: (
+            <div className="max-h-full overflow-y-scroll">
+                <SplitSettings />
+            </div>
+        ),
+    },
+    {
+        label: "Tabs",
+        key: "tabs",
+        children: <TabsSettings />,
+    },
+    {
+        label: "Window",
+        key: "window",
+        children: <WindowSettings />,
+    },
+];
+const previewTabs: Tab[] = [
+    {
+        label: "Chat",
+        key: "chat",
+        children: (
+            <div className="p-4 bg-gray-800 text-white h-full overflow-hidden flex flex-col">
+                <ChatterinoSplitVertical
+                    chatMessages={fakeChatListLarge}
+                    extraClasses={clsx("h-[1000px]")}
+                />
+            </div>
+        ),
+    },
+    {
+        label: "Split Drop Preivew",
+        key: "spilt_drop_preview",
+        children: (
+            <div className="p-4 bg-gray-800 text-white h-full overflow-hidden">
+                <div>
+                    Hover mouse around split & drop target to simulate dragging
+                    split
+                </div>
+                <ChatterinoDragSplitPreview chatMessages={fakeChatListSmall} />
+            </div>
+        ),
+    },
+    {
+        label: "Split Drop Target",
+        key: "spilt_drop_target",
+        children: (
+            <div className="p-4 bg-gray-800 text-white h-full overflow-hidden flex flex-col">
+                <div>
+                    Hover mouse around split & drop target to simulate dragging
+                    split
+                </div>
+                <ChatterinoSplitAdvanced chatMessages={fakeChatListVerySmall} />
+            </div>
+        ),
+    },
+    {
+        label: "Split Resize",
+        key: "spilt_resize",
+        children: (
+            <div className="p-4 bg-gray-800 text-white h-full overflow-hidden flex flex-col">
+                <ChatterinoSplitResize chatMessages={fakeChatListVerySmall} />
+            </div>
+        ),
+    },
+    {
+        label: "Tab States",
+        key: "tab_states",
+        children: (
+            <div className="p-4 bg-gray-800 h-full overflow-auto flex flex-col">
+                <ChatterinoTabPreview />
+            </div>
+        ),
+    },
+];
+
+function ThemeModal({
+    isOpen,
+    setOpen,
+}: {
+    isOpen: boolean;
+    setOpen: (v: boolean) => void;
+}) {
+    const { data, setData } = useConfigContext();
+    const [selectedPreset, setSelectedPreset] = useState("dark");
+    const [selectedFile, setSelectedFile] = useState<File | null>(null);
+    const fileInputRef = useRef<HTMLInputElement>();
+    return (
+        <Modal
+            open={isOpen}
+            afterOpenChange={(v) => setOpen(v)}
+            okButtonProps={{
+                type: "default",
+            }}
+            onOk={() => {
+                switch (selectedPreset) {
+                    case "light":
+                        setData(CHATTERINO_LIGHT_THEME);
+                        break;
+                    case "dark":
+                        setData(CHATTERINO_DARK_THEME);
+                        break;
+                    case "white":
+                        setData(CHATTERINO_WHITE_THEME);
+                        break;
+                    case "black":
+                        setData(CHATTERINO_BLACK_THEME);
+                        break;
+                    case "custom":
+                        if (!selectedFile) {
+                            alert("No file selected!");
+                            return;
+                        }
+                        selectedFile
+                            .text()
+                            .then((res) => {
+                                // TODO: extra no validation
+                                const theme = JSON.parse(res) as ThemeData;
+                                setData(qt2css(theme));
+                            })
+                            .catch((err) => {
+                                console.error("Error reading file", err);
+                                alert("Error reading file: " + err.message);
+                            });
+                }
+                setOpen(false);
+            }}
+            onCancel={() => setOpen(false)}
+        >
+            <h1 className="col-span-2 text-xl font-bold"> New Theme </h1>
+            <hr className="my-4" />
+            {/*<div className="flex items-center space-x-2">*/}
+            <h2 className="col-span-2 text-base font-semibold my-4">
+                From Chatterino Theme ...
+            </h2>
+            <div className="space-y-2">
+                {["light", "dark", "white", "black"].map((it) => (
+                    <button
+                        key={it}
+                        className={clsx(
+                            "w-full py-4 rounded-md border font-mono",
+                            selectedPreset == it
+                                ? "bg-gray-700 text-gray-100 border-gray-100 "
+                                : "bg-gray-100 text-gray-700 border-gray-700 "
+                        )}
+                        onClick={() => setSelectedPreset(it)}
+                    >
+                        {it.toUpperCase()}
+                    </button>
+                ))}
+            </div>
+            <h2 className="col-span-2 text-base font-semibold my-4">
+                Import from your existing theme
+            </h2>
+            <div className="space-y-2">
+                <button
+                    className={clsx(
+                        "w-full py-4 rounded-md border font-mono",
+                        selectedPreset == "custom"
+                            ? "bg-gray-700 text-gray-100 border-gray-100 "
+                            : "bg-gray-100 text-gray-700 border-gray-700 "
+                    )}
+                    onClick={() => {
+                        setSelectedPreset("custom");
+                        fileInputRef.current?.click();
+                    }}
+                >
+                    Choose file
+                </button>
+                {selectedPreset == "custom" && selectedFile == null && (
+                    <p className="my-1 text-red-500"> Please select a file </p>
+                )}
+                {selectedPreset == "custom" && selectedFile && (
+                    <p>
+                        <strong>{selectedFile.name}</strong> selected
+                    </p>
+                )}
+                <input
+                    type="file"
+                    className="hidden"
+                    ref={(it) => (fileInputRef.current = it!)}
+                    onChange={(e) => {
+                        const file = e.target?.files?.[0];
+                        if (!file) {
+                            setSelectedFile(null);
+                        } else {
+                            setSelectedFile(file);
+                        }
+                    }}
+                />
+            </div>
+        </Modal>
     );
 }
--- src/app/settings/overviewSettings.tsx
+++ src/app/settings/overviewSettings.tsx
@@ -1,27 +1,8 @@
 import { ColorPickerWrapper } from "@/app/settings/messageSettings";
-import { ThemeData, useConfigContext } from "@/app/color-context-provider";
-import { useCallback, useRef, useState } from "react";
+import { useConfigContext } from "@/app/color-context-provider";
+import { useCallback } from "react";
 import { produce } from "immer";
 import { Radio } from "antd";
-import clsx from "clsx";
-import {
-    CHATTERINO_BLACK_THEME,
-    CHATTERINO_DARK_THEME,
-    CHATTERINO_LIGHT_THEME,
-    CHATTERINO_WHITE_THEME,
-} from "@/resources";
-import { css2qt, qt2css } from "@/utils";
-
-function downloadFile(text: string, fileName: string) {
-    const a = document.createElement("a");
-    a.href = "data:text/plan;charset=utf-8," + encodeURIComponent(text);
-    a.download = fileName;
-    a.style.display = "none";
-
-    document.body.appendChild(a);
-    a.click();
-    document.removeChild(a);
-}
 
 export function OverviewSettings() {
     const { data, setData } = useConfigContext();
@@ -36,96 +17,9 @@ export function OverviewSettings() {
         },
         [setData]
     );
-    const [selectedPreset, setSelectedPreset] = useState("dark");
-    const fileInputRef = useRef<HTMLInputElement>();
 
     return (
         <div className="">
-            <h1 className="col-span-2"> start here </h1>
-            {/*<div className="flex items-center space-x-2">*/}
-            <div className="flex space-x-2">
-                {["light", "dark", "white", "black"].map((it) => (
-                    <button
-                        key={it}
-                        className={clsx(
-                            "w-[80px] py-4 rounded-md border font-mono",
-                            selectedPreset == it
-                                ? "bg-gray-700 text-gray-100 border-gray-100 "
-                                : "bg-gray-100 text-gray-700 border-gray-700 "
-                        )}
-                        onClick={() => setSelectedPreset(it)}
-                    >
-                        {it.toUpperCase()}
-                    </button>
-                ))}
-                <button
-                    className={clsx(
-                        "w-[80px] py-4 rounded-md border font-mono",
-                        selectedPreset == "custom"
-                            ? "bg-gray-700 text-gray-100 border-gray-100 "
-                            : "bg-gray-100 text-gray-700 border-gray-700 "
-                    )}
-                    onClick={() => {
-                        setSelectedPreset("custom");
-                        fileInputRef.current?.click();
-                    }}
-                >
-                    CUSTOM
-                </button>
-                <input type="file" ref={(it) => (fileInputRef.current = it!)} />
-            </div>
-
-            <button
-                className="font-mono my-2 px-2 py-1 border rounded-md"
-                onClick={() => {
-                    switch (selectedPreset) {
-                        case "light":
-                            setData(CHATTERINO_LIGHT_THEME);
-                            break;
-                        case "dark":
-                            setData(CHATTERINO_DARK_THEME);
-                            break;
-                        case "white":
-                            setData(CHATTERINO_WHITE_THEME);
-                            break;
-                        case "black":
-                            setData(CHATTERINO_BLACK_THEME);
-                            break;
-                        case "custom":
-                            const selectedFile =
-                                fileInputRef.current?.files?.[0];
-                            if (!selectedFile) {
-                                alert("No file selected!");
-                                return;
-                            }
-                            selectedFile
-                                .text()
-                                .then((res) => {
-                                    // TODO: extra no validation
-                                    const theme = JSON.parse(res) as ThemeData;
-                                    setData(qt2css(theme));
-                                    alert("applied: " + selectedFile.name);
-                                })
-                                .catch((err) => {
-                                    console.error("Error reading file", err);
-                                    alert("Error reading file: " + err.message);
-                                });
-                    }
-                }}
-            >
-                Apply &gt;
-            </button>
-            {/*</div>*/}
-
-            <button
-                className="font-mono my-2 px-2 py-1 border rounded-md"
-                onClick={() => {
-                    downloadFile(JSON.stringify(css2qt(data)), "theme.json");
-                }}
-            >
-                Export
-            </button>
-
             <hr className="col-span-2" />
 
             <div className="text-lg font-bold col-span-2">Icon theme</div>
