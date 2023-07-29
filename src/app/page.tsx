a9e849acd24930b33840ce8491b38c80e1da07dc
road <rod8711@gmail.com>
2023-07-30 01:09:48 +0700

    add footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import { ConfigContextProvider } from "@/app/color-context-provider";
 import { ColorApp } from "@/app/ColorApp";
 import { ThemeButtons } from "@/app/Home.widgets";
 import { TabContextProvider } from "@/app/tab-context-provider";
+import { FiGithub } from "react-icons/fi";
 
 export default function Home() {
     return (
@@ -15,10 +16,41 @@ export default function Home() {
                         </div>
                         <ThemeButtons />
                     </div>
-                    {/*overflow-auto somehow force the height to be correct??*/}
-                    <ColorApp />
+                    <div className="overflow-hidden flex-1">
+                        <ColorApp />
+                    </div>
+                    <Footer />
                 </div>
             </TabContextProvider>
         </ConfigContextProvider>
+    );
+}
+
+function Footer() {
+    return (
+        <div className="w-full bg-neutral-800 text-gray-400 px-4 py-2 mt-2 flex flex-wrap text-sm">
+            <div>
+                Chatterino Theme Creator is made by{" "}
+                <a
+                    href="https://github.com/rod41732"
+                    target="_blank"
+                    rel="noreferrer"
+                    className="underline"
+                >
+                    @rod41732
+                </a>
+            </div>
+            <div className="flex-grow"></div>
+            <div className="flex items-center">
+                <a
+                    href="https://github.com/rod41732/chatterino-theme-creator-web"
+                    target="_blank"
+                    rel="noreferrer"
+                >
+                    <FiGithub className="inline-block mr-2 text-xl" />
+                    GitHub
+                </a>
+            </div>
+        </div>
     );
 }
