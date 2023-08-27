bbc489ea676d9760d63ebeda3a0ab5bf0058fad9
road <rod8711@gmail.com>
2023-08-28 00:43:39 +0700

    fix preview doesn't actually show the theme

--- src/app/edit/[id]/page.tsx
+++ src/app/edit/[id]/page.tsx
@@ -21,6 +21,7 @@ import { produce } from "immer";
 import { copyToClipboard, downloadFile } from "@/lib/export-theme";
 import { css2qt } from "@/utils";
 import useNotification from "antd/es/notification/useNotification";
+import { ColorProvider } from "@/lib/ColorProvider";
 
 interface RouteParams {
     params: {
@@ -31,8 +32,8 @@ export default function EditThemePage({ params: { id } }: RouteParams) {
     const router = useRouter();
     const query = useSearchParams()!;
     const pathName = usePathname()!;
-    const isInfo = query.has("info");
     const isPreview = query.has("preview");
+    const { data } = useConfigContext();
 
     return id ? (
         <EditorStateContextProvider>
@@ -42,7 +43,7 @@ export default function EditThemePage({ params: { id } }: RouteParams) {
                         <div className="text-2xl font-bold py-2">
                             Chatterino Theme Creator
                         </div>
-                        {isInfo || isPreview ? (
+                        {isPreview ? (
                             <>
                                 <div className="flex-1"></div>
                                 {isPreview && (
@@ -64,15 +65,10 @@ export default function EditThemePage({ params: { id } }: RouteParams) {
                         )}
                     </div>
                     <div className="flex-1 overflow-hidden">
-                        {isInfo ? (
-                            <div className="h-full w-full flex flex-col overflow-hidden">
+                        {isPreview ? (
+                            <ColorProvider theme={data} className="h-full">
                                 <ChatterinoAllPreviews />
-                                <div className="flex-shrink-0">
-                                    <ThemeDetails themeId={id} />
-                                </div>
-                            </div>
-                        ) : isPreview ? (
-                            <ChatterinoAllPreviews />
+                            </ColorProvider>
                         ) : (
                             <ColorApp />
                         )}
@@ -83,96 +79,5 @@ export default function EditThemePage({ params: { id } }: RouteParams) {
         </EditorStateContextProvider>
     ) : (
         <div> no theme id</div>
-    );
-}
-
-function ThemeDetails({ themeId }: { themeId: string }) {
-    const { data } = useConfigContext();
-    const router = useRouter();
-    const idWithoutPrefix = themeId.split("-")[1];
-
-    const [notification, contentHolder] = useNotification();
-
-    return (
-        <div>
-            {contentHolder}
-            <div className="flex overflow-hidden">
-                <div>
-                    <span className="text-2xl font-bold">
-                        {data.ctcMeta.name}
-                    </span>
-                    <span className="text-gray-500"> By @forsen</span>
-                </div>
-                <div className="flex-1"></div>
-                <div className={"flex items-center gap-x-2"}>
-                    <IconButton
-                        tooltip="Download theme"
-                        onClick={() => {
-                            downloadFile(
-                                JSON.stringify(css2qt(data), null, 2),
-                                idWithoutPrefix +
-                                    "-" +
-                                    data.ctcMeta.name +
-                                    ".json",
-                            );
-                        }}
-                    >
-                        <MdDownload />
-                    </IconButton>
-                    <IconButton
-                        tooltip="Copy theme to clipboard"
-                        onClick={async () => {
-                            const themeJSON = JSON.stringify(data, null, 2);
-                            await copyToClipboard(themeJSON);
-                            notification.success({
-                                message: "Copied theme to clipboard",
-                            });
-                        }}
-                    >
-                        <MdContentCopy />
-                    </IconButton>
-                    <IconButton
-                        tooltip="Forsen aaaaaaaaaaaaaaaa "
-                        onClick={async () => {
-                            const dataCopy = produce(data, (draft) => {
-                                draft.ctcMeta.name += " Copy";
-                            });
-                            const themeId = createAndSaveTheme(dataCopy);
-                            await router.push(`/edit/${themeId}`);
-                        }}
-                    >
-                        <BiSolidDuplicate />
-                    </IconButton>
-                </div>
-            </div>
-
-            <div className="flex items-center gap-x-2 flex-wrap">
-                {
-                    // dark theme has light icons and vice-versa, hence the "inverted" logic
-                    data.metadata.iconTheme == "light" ? (
-                        <p className="px-2 py-1 rounded-md bg-gray-900 text-white border border-white leading-none">
-                            Dark
-                        </p>
-                    ) : (
-                        <p className="px-2 py-1 rounded-md bg-white text-gray-900 border border-gray-900 leading-none">
-                            Light
-                        </p>
-                    )
-                }
-                {data.ctcMeta.messageSeparator && (
-                    <p className="px-2 py-1 rounded-md bg-white text-blue-500 border border-blue-500 leading-none">
-                        Separator
-                    </p>
-                )}
-                {data.ctcMeta.checkeredRow && (
-                    <p className="px-2 py-1 rounded-md bg-blue-500 text-white border border-white leading-none">
-                        Checkered
-                    </p>
-                )}
-            </div>
-
-            <p className="text-gray-500"> Updated: {data.ctcMeta.modifiedAt}</p>
-            <p className="text-gray-500"> {themeId}</p>
-        </div>
     );
 }
