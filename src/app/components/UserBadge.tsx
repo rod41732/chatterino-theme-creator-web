9618a462758274de044fd9e274aabd811063e020
road <rod8711@gmail.com>
2023-08-28 14:17:28 +0700

    UserBadge: don't show "Not logged in" in loading state

--- src/app/components/UserBadge.tsx
+++ src/app/components/UserBadge.tsx
@@ -32,38 +32,42 @@ export function UserBadge() {
             </div>
         </Dropdown>
     ) : (
-        <Dropdown
-            menu={{
-                items: [
-                    {
-                        key: "hint",
-                        label: <p>Login to Share your theme with others.</p>,
-                        disabled: true,
-                    },
-                    {
-                        type: "divider",
-                    },
-                    {
-                        key: "login",
-                        label: "Login with GitHub",
-                        onClick: async () => {
-                            const fullUrl = location.href;
-                            const origin = new URL(fullUrl).origin;
-                            const path = fullUrl.slice(origin.length);
-
-                            localStorage.setItem("redirect-url", path);
-                            await router.push("/github-login/init");
+        auth && (
+            <Dropdown
+                menu={{
+                    items: [
+                        {
+                            key: "hint",
+                            label: (
+                                <p>Login to Share your theme with others.</p>
+                            ),
+                            disabled: true,
                         },
-                    },
-                ],
-            }}
-        >
-            <div className="flex items-center gap-x-2 rounded-md hover:bg-gray-500/25 py-2 px-2 cursor-pointer">
-                <div className="flex items-center justify-center w-8 h-8 bg-gray-500 rounded-full text-xl text-white">
-                    <MdPerson />
+                        {
+                            type: "divider",
+                        },
+                        {
+                            key: "login",
+                            label: "Login with GitHub",
+                            onClick: async () => {
+                                const fullUrl = location.href;
+                                const origin = new URL(fullUrl).origin;
+                                const path = fullUrl.slice(origin.length);
+
+                                localStorage.setItem("redirect-url", path);
+                                await router.push("/github-login/init");
+                            },
+                        },
+                    ],
+                }}
+            >
+                <div className="flex items-center gap-x-2 rounded-md hover:bg-gray-500/25 py-2 px-2 cursor-pointer">
+                    <div className="flex items-center justify-center w-8 h-8 bg-gray-500 rounded-full text-xl text-white">
+                        <MdPerson />
+                    </div>
+                    <div className="text-gray-800">Not Logged in</div>
                 </div>
-                <div className="text-gray-800">Not Logged in</div>
-            </div>
-        </Dropdown>
+            </Dropdown>
+        )
     );
 }
